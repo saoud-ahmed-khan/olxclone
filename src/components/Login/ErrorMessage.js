f9a028cf7a8538705c0f2e54fb689a694095a057
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-06-02 18:17:00 +0500

    checking

--- src/components/Login/ErrorMessage.js
+++ src/components/Login/ErrorMessage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export  function Error(props) {
     return (
-        <div style={{color:"red",marginBottom:"5%"}}>
+        <div style={{color:"red",marginBottom:"3%"}}>
         {props.children}
     </div>
     )
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -4,13 +4,38 @@ import * as Yup from "yup";
 import { AiOutlineUser } from "react-icons/ai";
 import { BiPhone } from "react-icons/bi";
 import { RiLockPasswordLine } from "react-icons/ri";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { ImAddressBook } from "react-icons/im";
 import { IoMdLogIn } from "react-icons/io";
+import { FaCity } from "react-icons/fa";
 import { Error } from "../Login/ErrorMessage";
 import "../Login/login.css";
+const options = [
+  {
+    value: "",
+    key: "Select City",
+  },
 
+  {
+    value: "karachi",
+    key: "karachi",
+  },
+  {
+    value: "lahore",
+    key: "lahore",
+  },
+  {
+    value: "queeta",
+    key: "queeta",
+  },
+];
 const initialValue = {
   phone: "",
   password: "",
+  username: "",
+  address: "",
+  passwordConfirmation: "",
+  city: "",
 };
 const submit = (values: any) => {
   console.log(`formdata ${values}`);
@@ -18,14 +43,21 @@ const submit = (values: any) => {
 const phoneRegExp = /^((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/;
 
 const validationSchema = Yup.object({
+  username: Yup.string().required("Username mising"),
+  address: Yup.string().required("address mising"),
+
   phone: Yup.string()
-    .required("required")
+    .required("Phone Number missing")
     .matches(phoneRegExp, "Phone number is not valid"),
-  password: Yup.string().required(" Password Required"),
+  password: Yup.string().required("Password missing"),
+  passwordConfirmation: Yup.string()
+    .required("Confirm Password missing")
+    .oneOf([Yup.ref("password"), null], "Passwords is not matching"),
+  city: Yup.string().required("City is required"),
 });
 export function Signup() {
   return (
-    <div className="form-login">
+    <div className="form-signup">
       <div>
         <h1>Sign Up</h1>
       </div>
@@ -37,19 +69,43 @@ export function Signup() {
         initialValues={initialValue}
       >
         <Form>
-        <div className="phone">
+          <div className="phone">
             <div className="phone-inner">
               <AiOutlineUser className="login-logo" size="35" />
               <Field type="text" id="username" name="username" />
             </div>
-            <ErrorMessage component={Error} name="phone" />
+            <ErrorMessage component={Error} name="username" />
           </div>
+
           <div className="phone">
             <div className="phone-inner">
               <BiPhone className="login-logo" size="35" />
               <Field type="text" id="phone" name="phone" />
             </div>
             <ErrorMessage component={Error} name="phone" />
+          </div>
+
+          <div className="phone">
+            <div className="phone-inner">
+              <FaCity className="login-logo" size="30" />
+              <Field className="select" as="select" id="city" name="city">
+                {options.map((option) => {
+                  return (
+                    <option key={option.value} value={option.value}>
+                      {option.key}
+                    </option>
+                  );
+                })}
+              </Field>
+            </div>
+            <ErrorMessage component={Error} name="city" />
+          </div>
+          <div className="phone">
+            <div className="phone-inner">
+              <ImAddressBook className="login-logo" size="35" />
+              <Field type="text" id="address" name="address" />
+            </div>
+            <ErrorMessage component={Error} name="address" />
           </div>
           <div className="password">
             <div className="phone-inner">
@@ -58,6 +114,18 @@ export function Signup() {
             </div>
             <ErrorMessage component={Error} name="password" />
           </div>
+          <div className="password">
+            <div className="phone-inner">
+              <RiLockPasswordFill size="35" className="login-logo" />
+              <Field
+                type="password"
+                id="passwordConfirmation"
+                name="passwordConfirmation"
+              />
+            </div>
+            <ErrorMessage component={Error} name="passwordConfirmation" />
+          </div>
+
           <div className="login-button">
             <IoMdLogIn size="35" className="login-logo" />
             <button onClick={submit} type="submit">
