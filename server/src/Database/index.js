3d9cc4353f8c9672d8802bc17d9b783139992840
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-07-06 00:43:36 +0500

    adding

--- server/src/Database/index.js
+++ server/src/Database/index.js
@@ -6,12 +6,14 @@ const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.M0NGODB_URI, {
       useUnifiedTopology: true,
-      useNewUrlParser: true,
+      useNewUrlParser: true,  
       useFindAndModify: false,
       useCreateIndex: true,
     });
+    console.log(process.env.M0NGODB_URI);
     console.log("mongo db connected successfully");
   } catch (e) {
+    console.log(process.env.JWT_SECRET);
 
     console.log(`unable to connect: ${e.message}`);
   }
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const { dbConnect } = require("./Database/index");
 const app = express();
+require("dotenv").config()
+
 
 
 app.use(express.json({ extended: false }));
